f344a6042bf115a8fb8e7a4b468049af9f644357
sreza1999 <50634191+sreza1999@users.noreply.github.com>
2019-05-19 14:31:47 +0430

    First comit
    download manager

---
+++ downloadManager/src/sample/model/Downloader.java
@@ -0,0 +1,24 @@
+package sample.model;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+import static com.sun.xml.internal.ws.developer.JAXWSProperties.CONNECT_TIMEOUT;
+
+public class Downloader {
+
+
+    public void download(String url) {
+
+
+    }
+
+
+}
+
+
+
+
+
---
+++ downloadManager/src/sample/model/File.java
@@ -0,0 +1,64 @@
+package sample.model;
+
+import java.net.URL;
+
+public class File {
+
+
+
+
+    private String name;
+    private double size;
+    private String date;
+    private URL url;
+    private String status;
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
