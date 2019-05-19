510114a45f097b1e73b3f9cbb18e978acda14294
salinor <ali.sadat79@gmail.com>
2019-05-19 15:04:28 +0430

    first ALi

---
+++ downloadManager/src/sample/model/search.java
@@ -0,0 +1,16 @@
+package sample.model;
+
+import java.util.Scanner;
+
+
+public class search {
+ public  void main(String[] args)
+ {
+     Scanner temp = new Scanner(System.in);
+      String NameForSearch = new String();
+ }
+
+
+
+  
+}
